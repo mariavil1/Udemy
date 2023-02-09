68fe978b00fda78fbcb2552322a2f17a80ac6567
mariavil1 <malu.001@hotmail.com>
2023-02-08 20:11:15 -0500

    terminando la calculadora

--- JAVASCRIPT/Leccion17/js/app.js
+++ JAVASCRIPT/Leccion17/js/app.js
@@ -1,5 +1,14 @@
 console.log('calculadora con bootstrap')
 
 function sumar(params) {
-    
+    const formulario = document.getElementById("formulario");
+    let operandoA = formulario['operandoA'];
+    let operandoB = formulario['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if (isNaN(resultado)) {
+        resultado = 'La operacion no incluye numeros';
+        
+    }
+    document.getElementById('resultado').innerHTML = resultado;
+
 }
